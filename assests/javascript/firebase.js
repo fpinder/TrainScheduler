2fb6964c42dc5fe45056b18d82897eedbd67520b
Frank P <fpinder@comcast.net>
2019-07-29 02:25:27 -0700

    added connection to firebase

--- assests/javascript/firebase.js
+++ assests/javascript/firebase.js
@@ -1,15 +1,13 @@
-// Document Ready Function
+// Your web app's Firebase configuration
 $(document).ready(function () {
-
-    // Your web app's Firebase configuration
     var firebaseConfig = {
-        apiKey: "AIzaSyCaVbmmLrPAZOUfgoilSGZx7B1zVTxjjS0",
-        authDomain: "fir-train-b2224.firebaseapp.com",
-        databaseURL: "https://fir-train-b2224.firebaseio.com",
-        projectId: "fir-train-b2224",
+        apiKey: "AIzaSyAdQCxhJwuFPuzMZuCcAdDi9otNySpvLe8",
+        authDomain: "train-schedule-e3b61.firebaseapp.com",
+        databaseURL: "https://train-schedule-e3b61.firebaseio.com",
+        projectId: "train-schedule-e3b61",
         storageBucket: "",
-        messagingSenderId: "824574635354",
-        appId: "1:824574635354:web:d9e08574899bfa77"
+        messagingSenderId: "298874427254",
+        appId: "1:298874427254:web:e188016361944e58"
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
@@ -18,42 +16,91 @@ $(document).ready(function () {
     var database = firebase.database();
 
     // Initial Values
-    var trainName = " My Train";
+    var trainName = "";
+    var TrainDestination = "";
+    var FirstTrain = "";
+    var TrainFrequency = "";
 
-    // Capture Button Click
-    $("#add-train").on("click", function (event) {
+    //capture buton click 
+    $("#add-train").on("click", function () {
 
         // Don't refresh the page!
         event.preventDefault();
 
-        trainName = $("#tName").val().trim();
+        var trainName = $("#tName").val().trim();
+        var TrainDestination = $("#destination").val().trim();
+        var FirstTrain = $("#fTrain").val().trim();
+        var TrainFrequency = $("#frequency").val().trim();
 
-        database.ref().set({
-            train: trainName
-        });
 
-        // Firebase watcher + initial loader HINT: .on("value")
-        database.ref().on("value", function (snapshot) {
 
-            // Log everything that's coming out of snapshot
-            console.log(snapshot.val());
-            console.log(snapshot.val().name);
-            // console.log(snapshot.val().email);
-            // console.log(snapshot.val().age);
-            // console.log(snapshot.val().comment);
+        // console.log("This is the Tarin name " + trainName)
+        // console.log("This is the TrainDestination " + TrainDestination)
+        //  console.log("This is the FirstTrain " + FirstTrain);
+        // console.log("This is the Train Frequency " + TrainFrequency)
 
-            // Change the HTML to reflect
-            $("#tName").text(snapshot.val().name);
-            // $("#email-display").text(snapshot.val().email);
-            // $("#age-display").text(snapshot.val().age);
-            // $("#comment-display").text(snapshot.val().comment);
-
-            // Handle the errors
-        }, function (errorObject) {
-            console.log("Errors handled: " + errorObject.code);
+        database.ref().push({
+            name: trainName,
+            Destination: TrainDestination,
+            FirstTrain: FirstTrain,
+            Frequency: TrainFrequency,
         });
 
     });
+    database.ref().on("child_added", function (snapshot) {
 
+        // Log everything that's coming out of snapshot
+        console.log(snapshot.val());
+        // console.log(snapshot.val().name);
+        // console.log(snapshot.val().Destination);
+        // console.log(snapshot.val().FirstTrain);
+        // console.log(snapshot.val().Frequency);
+
+        var trainName = snapshot.val().name;
+        var TrainDestination = snapshot.val().Destination;
+        var FirstTrain = snapshot.val().FirstTrain;
+        var TrainFrequency = snapshot.val().Frequency;
+
+
+        console.log("This is the FirstTrain " + FirstTrain);
+        var firstTrainMoment = moment(FirstTrain, 'HH:mm');
+        var nowMoment = moment(); //current date and time
+
+        console.log("firstTrainConverted " + firstTrainMoment)
+        console.log("current date and time " + nowMoment)
+
+        var minutesSinceFirstArrival = nowMoment.diff(firstTrainMoment, 'minutes');
+
+        console.log("minutes Since FirstArrival " + minutesSinceFirstArrival)
+
+        // Change the HTML to reflect the data 
+        var tr = ("<tr>");
+        var tn = ("<td>");
+        var td = ("<td>");
+
+        $("#newTrains").append(
+            "<tr><td>" + snapshot.val().name + "</td>" +
+            "<td>" + snapshot.val().Destination + "</td>" +
+            "<td>" + snapshot.val().Frequency + "</td>" +
+
+            "</tr>"
+        );
+
+        // tn.append(snapshot.val().name);
+
+
+        // tr.append(tn);
+
+        // $('#newTrains').append(tr);
+
+        // $("#name-display").text(snapshot.val().name);
+        // $("#email-display").text(snapshot.val().email);
+        // $("#age-display").text(snapshot.val().age);
+        // $("#comment-display").text(snapshot.val().comment);
+
+        // Handle the errors
+    }, function (errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    });
 
 });
